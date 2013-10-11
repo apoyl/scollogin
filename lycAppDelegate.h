8ffd674eb5bbfcafdd20ceebecabc89b0991f0c2
aotuman <lyc@aotumantekiMacBook-Pro.local>
2013-10-11 11:27:51 +0800

    Initial commit

---
+++ lycAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  lycAppDelegate.h
+//  scollogin
+//
+//  Created by aotuman on 13-8-22.
+//  Copyright (c) 2013年 aotuman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface lycAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+
+@end
